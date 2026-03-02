352ea68a2ae50483d3532df75ac165edeb0008d7
Biswajit <biswajitkr.dandapat@gmail.com>
2026-03-02 16:32:55 +0530

    DB creating

--- server.js
+++ server.js
@@ -13,3 +13,21 @@ app.listen(PORT, () => {
     console.log(`Server running port ${PORT}`);
 })
 
+const pool = require('./db');
+
+async function createTable() {
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS Contact (
+      id SERIAL PRIMARY KEY,
+      phoneNumber VARCHAR(20),
+      email VARCHAR(255),
+      linkedId INTEGER,
+      linkPrecedence VARCHAR(10) CHECK (linkPrecedence IN ('primary', 'secondary')),
+      createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      deletedAt TIMESTAMP
+    );
+  `);
+}
+
+createTable();
