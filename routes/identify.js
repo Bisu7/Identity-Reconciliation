575a0815f0ecf9dc2a93ed4e62de0f349cd1b4f8
Biswajit <biswajitkr.dandapat@gmail.com>
2026-03-02 16:08:18 +0530

    identifying logic

---
+++ routes/identify.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const identifyController = require('../controllers/identifyController');
+
+router.post('/',identifyController.identify);
+
